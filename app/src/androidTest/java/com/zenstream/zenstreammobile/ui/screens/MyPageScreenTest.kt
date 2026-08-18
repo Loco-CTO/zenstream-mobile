18ed7c3746fae424b7cf319d40a3ec017f33a47c
Loco-CTO <61045140+Loco-CTO@users.noreply.github.com>
2026-08-18 23:27:21 +0100

    refactor: remove unused import assertDoesNotExist from MyPageScreenTest

--- app/src/androidTest/java/com/zenstream/zenstreammobile/ui/screens/MyPageScreenTest.kt
+++ app/src/androidTest/java/com/zenstream/zenstreammobile/ui/screens/MyPageScreenTest.kt
@@ -1,7 +1,6 @@
 package com.zenstream.zenstreammobile.ui.screens
 
 import androidx.compose.ui.test.assertIsDisplayed
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
