b9144cd859c0f620b5dc4163f5f86a4dd61a9e2b
Loco-CTO <61045140+Loco-CTO@users.noreply.github.com>
2026-08-21 20:51:57 +0100

    refactor: remove unused assertDoesNotExist import in ContentScreensTest

--- app/src/androidTest/java/com/zenstream/zenstreammobile/ui/ContentScreensTest.kt
+++ app/src/androidTest/java/com/zenstream/zenstreammobile/ui/ContentScreensTest.kt
@@ -11,7 +11,6 @@ import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.test.assertDoesNotExist
 import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.getUnclippedBoundsInRoot
 import androidx.compose.ui.test.junit4.createComposeRule
